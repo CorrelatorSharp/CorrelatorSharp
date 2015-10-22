0ffd00d68ffb6e503e75d18f3363360fca72ecde
Ivan Zlatev <ivan@ivanz.com>
2015-10-22 21:01:51 +0100

    Add support for RestSharp

---
+++ CorrelatorSharp.RestSharp/IRestRequestCorrelationIdHeaderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using CorrelatorSharp;
+
+namespace RestSharp
+{
+    public static class IRestRequestCorrelationIdHeaderExtensions
+    {
+        public static void AddCorrelationHeader(this IRestRequest request)
+        {
+            request.AddHeader(Headers.CorrelationId, ActivityScope.Current?.Id ?? Guid.NewGuid().ToString());
+        }
+    }
+}
