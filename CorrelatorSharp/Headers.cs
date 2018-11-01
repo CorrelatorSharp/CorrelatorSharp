dac7d98f1d2690a50250c75687c070f4e2ea3ba1
Jason Dryhurst-Smith <jason@codat.io>
2018-11-01 14:52:07 +0000

    add the parent ID as a specified header/field value

--- CorrelatorSharp/Headers.cs
+++ CorrelatorSharp/Headers.cs
@@ -4,5 +4,7 @@
     {
         public static readonly string CorrelationId = "X-Correlation-Id";
         public static readonly string CorrelationName = "X-Correlation-Name";
+
+        public static readonly string CorrelationParentId = "X-Correlation-Parent-Id";
     }
 }
